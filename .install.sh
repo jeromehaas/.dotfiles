5ccf5068931287a80e270b15d89ece52757ae198
Jérôme Haas <jeromehaas@Jeromes-MacBook-Pro.local>
2020-12-21 09:43:48 +0100

    Add oh-my-zsh and comment install.sh file

--- .install.sh
+++ .install.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 ############################
-# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
-# And also installs Homebrew Packages
-# And sets Sublime preferences
+# SETUP DOTfILES
+# INSTALLS MAIN PROGRAMS 
 ############################
 
+# GIVE ONE PARAMETER (HOMEDIR) TO EXECUTE SCRIPT
 if [ "$#" -ne 1 ]; then
     echo "Usage: install.sh <home_directory>"
     exit 1
 fi
-
 homedir=$1
 
-# dotfiles directory
+# DEFINE DIRECOTY FOR DOTFILES
 dotfiledir=${homedir}/dotfiles
 
-# list of files/folders to symlink in ${homedir}
-files="aliases bash_profile bash_prompt bashrc gitconfig gitignore viminfo vimrc zsh_history zshrc vim viminfo CFUserTextEncoding git-completion.bash vscode viminfo oh-my-zsh"
+# LIST ALL FILES TO MAKE A SYMLINK FOR
+files="aliases bash_profile bash_prompt bashrc gitconfig gitignore viminfo vimrc zsh_history zshrc vim viminfo CFUserTextEncoding git-completion.bash vscode viminfo oh-my-zsh ssh"
 
-# change to the dotfiles directory
+# SWITCH TO DOTFILES DIRECTORY
 echo "Changing to the ${dotfiledir} directory"
 cd ${dotfiledir}
 echo "...done"
 
-# create symlinks (will overwrite old dotfiles)
+# CREATE FOR EACH FILES OR DIRECTORY A SYMLINK
+# WARNING: THIS WILL OVERWRITE YOUR OLD FILES
 for file in ${files}; do
     echo "Creating symlink to $file in home directory."
     ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
 done
 
-# Download Git Auto-Completion
+# INSTALL GIT AUTO COMPLETION
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${homedir}/.git-completion.bash
 
-# Run the Homebrew Script
+# OUTCOMMENT THE SCRIPT BELOW TO EXECUTE SCRIPT TO 
+# INSTALL SOME MAIN APPLICATIONS
 # ./.brew.sh
 
--- .zshrc
+++ .zshrc
@@ -8,7 +8,7 @@ export ZSH="/Users/jeromehaas/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="ys"
+ZSH_THEME="robbyrussell"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -22,8 +22,6 @@ ZSH_THEME="ys"
 # Uncomment the following line to use hyphen-insensitive completion.
 # Case-sensitive completion must be off. _ and - will be interchangeable.
 # HYPHEN_INSENSITIVE="true"
-
-export ZSH_DISABLE_COMPFIX=true 
 
 # Uncomment the following line to disable bi-weekly auto-update checks.
 # DISABLE_AUTO_UPDATE="true"
@@ -73,7 +71,7 @@ export ZSH_DISABLE_COMPFIX=true
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
-source ~/.aliases
+
 # User configuration
 
 # export MANPATH="/usr/local/man:$MANPATH"
